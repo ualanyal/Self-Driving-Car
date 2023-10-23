489f6dd3bc84db44bc5713ba3a8a46b622e837a3
ualanyal <ualanyal@gmail.com>
2023-10-23 21:36:56 +0200

    - movement of the car - lanes

--- controls.js
+++ controls.js
@@ -1,46 +1,45 @@
 class Controls{
     constructor(){
-        this.forward=false;
-        this.left=false;
-        this.right=false;
-        this.reverse=false;
-
+        this.forward = false;
+        this.left = false;
+        this.right = false;
+        this.reverse = false;
 
         this.#addKeyboardListeners();
     }
 
-    #addKeyboardListeners(){
-        document.onkeydown=(event)=>{
+    #addKeyboardListeners() {
+        document.onkeydown = (event) => {
             switch(event.key){
                 case "ArrowLeft":
-                    this.left=true;
+                    this.left = true;
                     break;
                 case "ArrowRight":
-                    this.right=true;
-                    break;
-                case "ArrowDown":
-                    this.reverse=true;
+                    this.right = true;
                     break;
                 case "ArrowUp":
-                    this.foward=true;
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
             }
-            console.table(this);
         }
-        document.onkeyup=(event)=>{
+        document.onkeyup = (event) => {
             switch(event.key){
                 case "ArrowLeft":
-                    this.left=false;
+                    this.left = false;
                     break;
                 case "ArrowRight":
-                    this.right=false;
-                    break;
-                case "ArrowDown":
-                    this.reverse=false;
+                    this.right = false;
                     break;
                 case "ArrowUp":
-                    this.foward=false;
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
             }
-            console.table(this);
         }
     }
 }
